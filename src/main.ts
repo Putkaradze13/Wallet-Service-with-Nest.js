b8bc44c70f9f4c276de1d62fd72d3140d71e4ead
“Tengiz <t.putkaradze@artjoker.team>
2022-06-07 11:09:42 +0400

    updated rabbitmq service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    Logger.log(`Server is running on port ${process.env.PORT}`);
   });
 }
 bootstrap();
